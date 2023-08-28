428ab4f8c2d0cce6a017c411ab7ecea15d3de6cb
brckd <brycked@proton.me>
2023-08-28 23:25:59 +0200

    chore: init astro

--- tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsup";
-import config from "@bricked/tsup-config";
-
-export default defineConfig({
-  ...config,
-  entry: ["src/**/*.ts"],
-  tsconfig: "src/tsconfig.json",
-});
